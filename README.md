9d0be72dd52cd9fb15df7228a8db2b1e2d48c26f
john <NyanSatan@users.noreply.github.com>
2021-02-11 17:12:40 +0300

    Added ability to build with custom ARM toolchains

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example of building with a different ARM toolchain
+
+```noone@Mac-mini-noone checkm8_bootkit % TOOLCHAIN=/opt/gcc-arm-none-eabi-7-2018-q2-update/bin ARM_CC=arm-none-eabi-gcc ARM_OBJCOPY=arm-none-eabi-objcopy make```
--- src/libbootkit/payload/rules.mk
+++ src/libbootkit/payload/rules.mk
@@ -1,9 +1,13 @@
-ARM_CC = $(TOOLCHAIN)/clang
+ARM_CC ?= clang
+ARM_OBJCOPY ?= llvm-objcopy
+
+
+ARM_CC_FULL = $(TOOLCHAIN)/$(ARM_CC)
 ARM_CFLAGS = -mthumb
 ARM_CFLAGS += -march=armv7-a
 ARM_CFLAGS += -fno-builtin -nostdlib
 
-ARM_OBJCOPY = $(TOOLCHAIN)/llvm-objcopy
+ARM_OBJCOPY_FULL = $(TOOLCHAIN)/$(ARM_OBJCOPY)
 
 
 PAYLOAD_DIR = $(LIBBOOTKIT_DIR)/payload
@@ -17,8 +21,8 @@ PAYLOAD_OBJECT = $(addprefix $(BUILD_PATH)/, $(PAYLOAD_SRC:.S=.o))
 $(PAYLOAD_BIN): $(PAYLOAD_OBJECT)
 	@echo "\tbuilding payload"
 	@$(DIR_HELPER)
-	@$(ARM_OBJCOPY) -O binary $< $@
+	@$(ARM_OBJCOPY_FULL) -O binary $< $@
 
 $(PAYLOAD_OBJECT): $(PAYLOAD_SRC)
 	@$(DIR_HELPER)
-	@$(ARM_CC) $(ARM_CFLAGS) -o $@ $<
+	@$(ARM_CC_FULL) $(ARM_CFLAGS) -o $@ $<
